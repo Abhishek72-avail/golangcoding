1c75a0476ca5caebee705d7642167efda467196f
abhishek <abhishekprasad7250@gmail.com>
2025-02-26 09:41:58 +0530

    code add

--- hello.go
+++ hello.go
@@ -1,10 +1,18 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello, World!")
+// 	fmt.Println("welcome to Go programming")
+// 	fmt.Println("This is my first Go program")
+// 	fmt.Println("I am learning Go programming")
+// }
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("welcome to Go programming")
-	fmt.Println("This is my first Go program")
-	fmt.Println("I am learning Go programming")
+func main(){
+	fmt.Println("abhishek prasasd")
 }
