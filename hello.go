0809d3ae5b55b59c3567b813b70d3ef64589f933
abhishek <abhishekprasad7250@gmail.com>
2025-02-26 09:28:04 +0530

    first code of go lang

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("welcome to Go programming")
+	fmt.Println("This is my first Go program")
+	fmt.Println("I am learning Go programming")
+}
